ca35aabba992d6876cf1d1f12ab35343047d2dee
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-01-10 01:55:37 +0530

    seo added

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,18 @@
+const siteMetadata = {
+  title: "Fitness Tracker",
+  author: "Sujal",
+  headerTitle: "Fitness Tracker Website",
+  description:
+    "Kickstart your fitness journey with our comprehensive Fitness Tracker. Track body weight, log workouts, and achieve your fitness goals efficiently. Join now to personalize your wellness plan and stay motivated on your path to a healthier lifestyle.",
+  language: "en-us",
+  siteUrl: "https://fitnesstrackernext.vercel.app", // your website URL
+  siteLogo: "/android-chrome-192x192",
+  socialBanner: "/website_poster.webp", // add social banner in the public folder
+  email: "sujalvanjaredev@gmail.com",
+  github: "https://github.com/Sujal-Vanjare",
+  twitter: "https://twitter.com/sujal169",
+  linkedin: "https://www.linkedin.com/in/sujal-vanjare-5195152a0/",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
